9a9336c1f937b556ca824bb21dc30f8d061fc1b1
Brandon Harris <brandon@relentnet.com>
2025-02-10 15:08:12 -0600

    Got it to correctly fetch catalog information and put it into my own API.

---
+++ src/app/api/catalog/route.js
@@ -0,0 +1,26 @@
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
+
+export const runtime = 'nodejs';
+
+import { NextResponse } from 'next/server';
+import { SquareClient, SquareEnvironment } from 'square';
+
+const client = new SquareClient({
+    environment: SquareEnvironment.Production,
+    token: process.env.SQUARE_ACCESS_TOKEN,
+});
+
+export async function GET() {
+    try {
+        const response = await client.catalog.list({});
+        console.log('response', response);
+
+        //const safeResult = response.result ?? [];
+        return NextResponse.json(response);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Failed to fetch catalog' }, { status: 500 });
+    }
+}
