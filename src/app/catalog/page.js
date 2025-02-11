8770e27b5af61d6803e24f4d6f68ba346a9ba0b8
Brandon Harris <brandon@relentnet.com>
2025-02-10 20:59:47 -0600

    Styled the catalog page.

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -39,27 +39,32 @@ export default function CatalogPage() {
     return (
         <div style={{ padding: '2rem' }}>
             <h1>Catalog Items</h1>
-            <ul>
+            <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 '>
                 {catalog.map((item) => {
                     const name = item.itemData?.name || 'Unnamed Item';
                     // Generate a slug from the product name and combine it with the ID.
                     const slug = slugify(name);
                     const href = `/catalog/${slug}-${item.id}`;
                     return (
-                        <li key={item.id} style={{ marginBottom: '1rem' }}>
-                            <h2>
-                                <Link href={href}>{name}</Link>
-                            </h2>
-                            <p>{item.itemData?.description || 'No Description'}</p>
+                        <Link href={href} key={item.id} className='relative group block'>
+                            <div className='flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 absolute inset-0 h-full w-full text-white bg-zinc-950/80 p-4 rounded shadow-md transition duration-350'>
+                                <h2 className='text-xl font-semibold'>
+                                    {name}
+                                </h2>
+                                <p>{item.itemData?.description || 'No Description'}</p>
+                                <p>
+                                    ${item.itemData?.variations?.[0]?.itemVariationData?.priceMoney?.amount / 100 || 'N/A'} USD
+                                </p>
+                            </div>
                             {item.imageUrl ? (
-                                <img src={item.imageUrl} alt={name} style={{ maxWidth: '200px' }} />
+                                <img src={item.imageUrl} alt={name} className='rounded' />
                             ) : (
                                 <p>No image available</p>
                             )}
-                        </li>
+                        </Link>
                     );
                 })}
-            </ul>
+            </div>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
     return (
-        <nav className='fixed h-16 flex items-center justify-center space-x-4 top-0 left-0 w-full bg-zinc-900 text-white'>
+        <nav className='fixed z-50 h-16 flex items-center justify-center space-x-4 top-0 left-0 w-full bg-zinc-900 text-white'>
             <NavbarItem href='/' label='Home' />
             <NavbarItem href='/catalog' label='Shop' />
             <NavbarItem href='/cart' label='Cart' />
